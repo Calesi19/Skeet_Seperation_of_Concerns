daecb1daedb8f212af11d04001f675738d261552
mickfish96 <mickfish1996@gmail.com>
2023-02-24 17:55:28 -0700

    added stubs of all draws

--- interface.cpp
+++ interface.cpp
@@ -1,2 +1,76 @@
 #include "interface.h"
 
+Interface::Interface()
+{
+
+}
+
+void Interface::input(UserInput input)
+{
+
+}
+
+void Interface::processing()
+{
+
+}
+
+void Interface::output()
+{
+
+}
+
+void InterfacePellet::draw(/*StorageElement element*/)
+{
+
+}
+
+void InterfaceMissile::draw(/*StorageElement element*/)
+{
+
+}
+
+void InterfaceBomb::draw(/*StorageElement element*/)
+{
+
+}
+
+void InterfaceShrapnel::draw(/*StorageElement element*/)
+{
+
+}
+
+void InterfaceFragment::draw(/*StorageElement element*/)
+{
+
+}
+
+void InterfaceExhaust::draw(/*StorageElement element*/)
+{
+
+}
+
+void InterfaceStreek::draw(/*StorageElement element*/)
+{
+
+}
+
+void InterfaceStandard::draw(/*StorageElement element*/)
+{
+
+}
+
+void InterfaceSinker::draw(/*StorageElement element*/)
+{
+
+}
+
+void InterfaceFloater::draw(/*StorageElement element*/)
+{
+
+}
+
+void InterfaceCrazy::draw(/*StorageElement element*/)
+{
+
+}
--- interface.h
+++ interface.h
@@ -22,65 +22,65 @@ public:
 class InterfacePellet : InterfaceElement
 {
 public:
-   virtual void draw(/*StorageElement element*/);
+   void draw(/*StorageElement element*/);
 };
 
 class InterfaceMissile : InterfaceElement
 {
 public:
-   virtual void draw(/*StorageElement element*/);
+   void draw(/*StorageElement element*/);
 };
 
 class InterfaceBomb : InterfaceElement
 {
 public:
-   virtual void draw(/*StorageElement element*/);
+   void draw(/*StorageElement element*/);
 };
 
 class InterfaceShrapnel : InterfaceElement
 {
 public:
-   virtual void draw(/*StorageElement element*/);
+   void draw(/*StorageElement element*/);
 };
 
 class InterfaceFragment : InterfaceElement
 {
 public:
-   virtual void draw(/*StorageElement element*/);
+   void draw(/*StorageElement element*/);
 };
 
 class InterfaceExhaust : InterfaceElement
 {
 public:
-   virtual void draw(/*StorageElement element*/);
+   void draw(/*StorageElement element*/);
 };
 
 class InterfaceStreek : InterfaceElement
 {
 public:
-   virtual void draw(/*StorageElement element*/);
+   void draw(/*StorageElement element*/);
 };
 
 class InterfaceStandard : InterfaceElement
 {
 public:
-   virtual void draw(/*StorageElement element*/);
+   void draw(/*StorageElement element*/);
 };
 
 class InterfaceSinker: InterfaceElement
 {
 public:
-   virtual void draw(/*StorageElement element*/);
+   void draw(/*StorageElement element*/);
 };
 
 class InterfaceFloater : InterfaceElement
 {
 public:
-   virtual void draw(/*StorageElement element*/);
+   void draw(/*StorageElement element*/);
 };
 
 class InterfaceCrazy : InterfaceElement
 {
 public:
-   virtual void draw(/*StorageElement element*/);
+   void draw(/*StorageElement element*/);
 };
